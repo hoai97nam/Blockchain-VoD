17cf20d259247a45c8a2f18b2fb28ec448e50c7c
hoai97nam <nhnam97@gmail.com>
2019-07-31 19:36:21 +0700

    initialization

---
+++ web3js_intro.js
@@ -0,0 +1,7 @@
+const Web3 = require ('web3');
+const rpcURL = "https://mainnet.infura.io/v3/922a32f36e0e43d0af08e33e01e718c0";
+const web3  = new Web3(rpcURL);
+const account = "0x2421477f9AC767160712e0435E5f9DDA6168008d";
+web3.eth.getBalance(account, (err, wei) => {
+    balance = web3.utils.fromWei(wei, 'ether')
+  })
