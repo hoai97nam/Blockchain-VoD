f275ddb7831db6c597442f544133e241080d557c
hoai97nam <nhnam97@gmail.com>
2019-08-04 11:24:53 +0700

    add: out put of balance

--- web3js_intro.js
+++ web3js_intro.js
@@ -4,4 +4,5 @@ const web3  = new Web3(rpcURL);
 const account = "0x2421477f9AC767160712e0435E5f9DDA6168008d";
 web3.eth.getBalance(account, (err, wei) => {
     balance = web3.utils.fromWei(wei, 'ether')
-  })
+  });
+console.log(balance);
